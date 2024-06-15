8196a4b9e44312cc859911efab732b1753298898
PrimaPangasa18 <primapangasa52@gmail.com>
2024-06-15 21:37:30 +0700

    Making routes and few views

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,46 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\CourseVideoController;
+use App\Http\Controllers\FrontController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SubscribeTransactionController;
+use App\Http\Controllers\TeacherController;
+use App\Models\SubscribeTransaction;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [FrontController::class, 'index'])->name('front.index');
+
+// domain.com/details/misal:html-css/laravel10. Terrgantung kelas yang dipilih
+Route::get('/details/{course/slug}', [FrontController::class, 'details'])->name('front.details');
+Route::get('/category/{category/slug}', [FrontController::class, 'category'])->name('front.category');
+
+Route::get('/pricing', [FrontController::class, 'pricing'])->name('front.pricing');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// Rute buat harus login terlebih dahulu sebelum mengakses yang lain karena ada fungsi group
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/checkout', [FrontController::class, 'checkout'])->name('front.checkout');
+    Route::get('/checkout_store', [FrontController::class, 'checkout_store'])->name('front.checkout.store');
+
+
+
+
+    Route::prefix('admin')->name('admin.')->group(function () {
+        Route::resource('categories', CategoryController::class)->middleware('role:owner'); //admin.categories.index/show/delete
+        Route::resource('teachers', TeacherController::class)->middleware('role:owner');
+        Route::resource('courses', CourseController::class)->middleware('role:owner|teacher');
+        Route::resource('subscribe_transactions', SubscribeTransactionController::class)->middleware('role:owner');
+        Route::resource('course_videos', CourseVideoController::class)->middleware('role:owner|teacher');
+    });
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
